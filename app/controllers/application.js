329acdd0ca2f01b834fc20fcdb4a298496b806fa
Ignacio Rivas <rivasign@gmail.com>
2015-11-08 19:30:48 +0000

    Add base components and fix up base layou

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,11 +4,11 @@ export default Ember.Controller.extend({
   isMenuOpen: false,
 
   init() {
-    $('body').on('click', (e) => {
+    Ember.$('body').on('click', (e) => {
       e.preventDefault();
 
-      if (this.isMenuOpen && $(e.target).parents('.st-menu').length === 0) {
-        $('#js-container').removeClass('st-menu-open');
+      if (this.isMenuOpen && Ember.$(e.target).parents('.st-menu').length === 0) {
+        Ember.$('#js-container').removeClass('st-menu-open');
         this.isMenuOpen = false;
       }
     });
@@ -16,7 +16,7 @@ export default Ember.Controller.extend({
 
   actions: {
     onMenuOpen(e) {
-      $('#js-container').addClass('st-effect-3 st-menu-open');
+      Ember.$('#js-container').addClass('st-effect-3 st-menu-open');
       this.isMenuOpen = true;
     }
   }
