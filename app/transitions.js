928ff6b02855b009a8b55fc2ace9a105af19bb67
Ignacio Rivas <rivasign@gmail.com>
2015-12-08 11:14:09 +0000

    Add fancy explode animations

--- app/transitions.js
+++ app/transitions.js
@@ -1,8 +1,13 @@
 export default function(){
+  const duration = 500;
+
   this.transition(
     this.fromRoute('movies.index'),
     this.toRoute('movies.show'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.use('explode', {
+      matchBy: 'data-poster-image',
+      use: ['fly-to', { duration }]
+    }),
+    this.reverse('toRight', { duration })
   );
 };
