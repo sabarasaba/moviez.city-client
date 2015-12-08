2e21da8213a1d22c1937701fae92b98dc94e750a
Ignacio Rivas <rivasign@gmail.com>
2015-12-08 10:32:58 +0000

    Add animation to scroll up

--- app/routes/movies/show.js
+++ app/routes/movies/show.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.find('movie', params.slug);
+  },
+
+  activate() {
+    Ember.$('html').velocity('scroll', {
+      offset: 0,
+      duration: 500
+    });
   }
 });
